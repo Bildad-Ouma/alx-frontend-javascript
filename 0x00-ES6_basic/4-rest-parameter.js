b09c63dad9414b60324c7c92ffd1f3320e3a90f1
Bildad-Ouma <sirbildadbrian@gmail.com>
2024-03-07 10:12:17 +0000

    4-rest-parameter.js

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,6 @@
+export default function returnHowManyArguments(...args) {
+  return args.length;
+}
+
+console.log(returnHowManyArguments('one'));
+console.log(returnHowManyArguments('one', 'two', 3, '4th'));
