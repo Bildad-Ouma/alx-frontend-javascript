6889cdf512ccbc66b6ad116524aa594a3f4fe2e6
Bildad-Ouma <sirbildadbrian@gmail.com>
2024-03-07 22:52:00 +0000

    4. Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,47 @@
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = null;
+    this._currency = null;
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('Both amount and conversionRate must be numbers');
+  }
+}
+
+export default Pricing;
