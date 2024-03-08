c61af67abce1f0c1a46ba20f94c8125e0fe52829
Bildad-Ouma <sirbildadbrian@gmail.com>
2024-03-08 03:00:33 +0000

    10. Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,21 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    const [photo, user] = await Promise.all([photoPromise, userPromise]);
+
+    return {
+      photo,
+      user
+    };
+  } catch (error) {
+    // If one of the async functions fails, return an empty object
+    return {
+      photo: null,
+      user: null
+    };
+  }
+}
